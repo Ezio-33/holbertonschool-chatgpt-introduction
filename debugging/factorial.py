0b7bf470a2990313b264ef086af9d8b40fd6de32
Ezio-33 <8691@holbertonstudents.com>
2024-05-07 01:04:46 +0200

    task 0

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -2,14 +2,22 @@
 import sys
 
 def factorial(n):
+    if n < 0:
+        return -1
     result = 1
-    while n > 1:
+    while n > 0:
         result *= n
-        n = n - 1
+        n -= 1
     return result
 
-if len(sys.argv) > 1:
-    f = factorial(int(sys.argv[1]))
-    print(f"The factorial of {sys.argv[1]} is: {f}")
-else:
-    print("Please provide an argument.")
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 factorial.py <number>")
+        sys.exit(1)
+
+    n = int(sys.argv[1])
+    result = factorial(n)
+    if result == -1:
+        print("Le nombre doit être supérieur ou égal à zéro.")
+    else:
+        print(f"Le facteuriel de {n} est {result}.")
