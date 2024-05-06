5a071096242f956b0b7e640104fc37f95727c6ce
Ezio-33 <8691@holbertonstudents.com>
2024-05-07 01:54:54 +0200

    task 3

--- debugging/mines.py
+++ debugging/mines.py
@@ -1,79 +1,54 @@
 #!/usr/bin/python3
 import random
-import os
-
-def clear_screen():
-    os.system('cls' if os.name == 'nt' else 'clear')
 
 class Minesweeper:
     def __init__(self, width=10, height=10, mines=10):
         self.width = width
         self.height = height
         self.mines = set(random.sample(range(width * height), mines))
+        self.board = [[' ' for _ in range(width)] for _ in range(height)]
         self.total_non_mine_cells = width * height - mines
-        self.field = [[' ' for _ in range(width)] for _ in range(height)]
-        self.revealed = [[False for _ in range(width)] for _ in range(height)]
         self.non_mine_cells_revealed = 0
 
     def print_board(self, reveal=False):
-        clear_screen()
-        print('  ' + ' '.join(str(i) for i in range(self.width)))
+        print('  ', end='')
+        for i in range(self.width):
+            print(i, end=' ')
+        print()
         for y in range(self.height):
             print(y, end=' ')
             for x in range(self.width):
-                if reveal or self.revealed[y][x]:
-                    if (y * self.width + x) in self.mines:
-                        print('*', end=' ')
-                    else:
-                        count = self.count_mines_nearby(x, y)
-                        print(count if count > 0 else ' ', end=' ')
-                else:
+                if reveal or (x, y) in self.mines:
                     print('.', end=' ')
+                else:
+                    print(self.board[y][x], end=' ')
             print()
 
-    def count_mines_nearby(self, x, y):
-        count = 0
-        for dx in [-1, 0, 1]:
-            for dy in [-1, 0, 1]:
-                nx, ny = x + dx, y + dy
-                if 0 <= nx < self.width and 0 <= ny < self.height:
-                    if (ny * self.width + nx) in self.mines:
-                        count += 1
-        return count
-
     def reveal(self, x, y):
-        if (y * self.width + x) in self.mines:
+        if (x, y) in self.mines:
             return False
-        self.revealed[y][x] = True
+        count = sum(1 for i in range(max(0, x-1), min(self.width, x+2))
+                     for j in range(max(0, y-1), min(self.height, y+2))
+                     if (i, j) in self.mines)
+        self.board[y][x] = count
         self.non_mine_cells_revealed += 1
-        if self.count_mines_nearby(x, y) == 0:
-            for dx in [-1, 0, 1]:
-                for dy in [-1, 0, 1]:
-                    nx, ny = x + dx, y + dy
-                    if 0 <= nx < self.width and 0 <= ny < self.height and not self.revealed[ny][nx]:
-                        self.reveal(nx, ny)
-        if self.non_mine_cells_revealed == self.total_non_mine_cells:
-            return True
         return True
 
     def play(self):
         while True:
             self.print_board()
             try:
-                x = int(input("Enter x coordinate: "))
-                y = int(input("Enter y coordinate: "))
+                x = int(input("Entrez la coordonnée x: "))
+                y = int(input("Entrez la coordonnée y: "))
                 if self.reveal(x, y):
                     if self.non_mine_cells_revealed == self.total_non_mine_cells:
                         self.print_board(reveal=True)
-                        print("Congratulations! You have won the game.")
+                        print("Félicitations ! Vous avez gagné le jeu.")
                         break
-                else:
-                    self.print_board(reveal=True)
-                    print("Game Over! You hit a mine.")
-                    break
             except ValueError:
-                print("Invalid input. Please enter numbers only.")
+                print("Veuillez entrer des coordonnées valides.")
 
-if __name__ == "__main__":
-    game = Minesweeper()
-    game.play()
+
+# Exemple d'utilisation
+minesweeper = Minesweeper()
+minesweeper.play()
